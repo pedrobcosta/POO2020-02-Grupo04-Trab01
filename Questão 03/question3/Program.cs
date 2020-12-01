7acb2dea90134ce2741fe04fe73d9d9bbd741859
pedroeborges <48721021+pedroeborges@users.noreply.github.com>
2020-12-01 15:31:11 -0300

    Questão 03

---
+++ Questão 03/question3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            String hora, min, secs;
+            
+            //T1
+            Console.WriteLine("Digite a hora, minutos e segundos do primeiro tempo: ");
+            hora = Console.ReadLine();
+            min = Console.ReadLine();
+            secs = Console.ReadLine();
+            Tempo t1 = new Tempo(hora, min, secs);
+            t1.Leitura();
+           
+            //T2
+            Console.WriteLine("Digite a hora, minutos e segundos do segundo tempo: ");
+            hora = Console.ReadLine();
+            min = Console.ReadLine();
+            secs = Console.ReadLine();
+            Tempo t2 = new Tempo(hora, min, secs);
+            t2.Leitura();
+          
+            //T3
+            Tempo t3 = new Tempo();
+            t3 = t1.Soma(t2); //Somatorio dos hora
+            Console.WriteLine("\nA soma dos tempos eh: ");
+            t3.Leitura(); 
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Questão 03/question3/Tempo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace question3
+{
+    class Tempo
+    {
+        //ATRIBUTOS
+        private int hora;
+        private int min;
+        private int secs;
+
+        //CONSTRUTORES
+        //letra A)
+        public Tempo() //Construtor default
+        {
+            hora = 0;
+            min = 0;
+            secs = 0;
+        } 
+
+        public Tempo(String _hora, String _min, String _secs) //Construtor entrada
+        {
+            hora = Convert.ToInt32(_hora);
+            min = Convert.ToInt32(_min);
+            secs = Convert.ToInt32(_secs);
+        }
+       
+        //MÉTODOS
+        //letra B)
+        public void Leitura() // Saida
+        {
+            
+            while (secs >= 60)
+            {
+                secs -= 60;
+                min += 1;
+            }
+            while (min >= 60)
+            {
+                min -= 60;
+                hora += 1;
+            }
+
+            Console.WriteLine($"{ hora: 00}:{ min: 00}:{ secs: 00}");
+        }
+
+        //letra C)
+        public Tempo Soma(Tempo t2)
+        {
+            Tempo t3 = new Tempo();
+            t3.hora = hora + t2.hora;
+            t3.min  = min + t2.min;
+            t3.secs = secs + t2.secs;
+
+            return t3;
+        }
+    }
+}
