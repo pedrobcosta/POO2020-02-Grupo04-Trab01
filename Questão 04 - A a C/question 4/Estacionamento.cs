ee44324eda334f0f84b618e5c2f64db885a27ba9
pedroeborges <48721021+pedroeborges@users.noreply.github.com>
2020-12-01 15:32:20 -0300

    Questão 04 - A a C

---
+++ Questão 04 - A a C/question 4/Estacionamento.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace question_4
+{
+    class Estacionamento
+    {
+        public Estacionamento() //Default
+        {
+
+        }
+
+        private String nCarro, nMarca;
+
+        public String getnCarro()
+        {
+            return (nCarro);
+        }
+        public String getMarca()
+        {
+            return (nMarca);
+        }
+
+        public void setnCarro(String _nCarro)
+        {
+            nCarro = _nCarro;
+        }
+        public void setMarca(String _nMarca)
+        {
+            nMarca = _nMarca;
+        }
+
+        public void saida() //saida
+        {
+            Console.WriteLine("\n-------------------\n");
+            Console.WriteLine("Numero do chassi do carro: " + nCarro);
+            Console.WriteLine("Marca do carro: " + nMarca);
+
+        }
+
+    }
+}
---
+++ Questão 04 - A a C/question 4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estacionamento p1 = new Estacionamento();
+            Tempo e1 = new Tempo();
+            Tempo s1 = new Tempo();
+
+            Console.WriteLine("Digite o número da chapa do carro: ");
+            p1.setnCarro(Console.ReadLine());
+
+            Console.WriteLine("Digite a marca do carro: ");
+            p1.setMarca(Console.ReadLine());
+
+            Console.WriteLine("Digite o horario de entrada(hh:mm:secs): ");
+            e1.setHora(Console.ReadLine());
+            e1.setMin(Console.ReadLine());
+            e1.setSecs(Console.ReadLine());
+
+            Console.WriteLine("Digite o horario de saida(hh:mm:secs): ");
+            s1.setHora(Console.ReadLine());
+            s1.setMin(Console.ReadLine());
+            s1.setSecs(Console.ReadLine());
+
+            //Questão b
+            p1.saida();
+            Console.WriteLine("Horário de Entrada: ");
+            e1.leitura();
+            Console.WriteLine("Horário de saída: ");
+            s1.leitura();
+
+            //Questão c
+            Tempo c1 = new Tempo();
+            c1 = s1.sub(e1);
+            Console.WriteLine("Horário de permanencia no estacionamento: ");
+            c1.leitura();
+            c1.cobrar();
+
+            Console.ReadLine();
+
+        }
+    }
+}
