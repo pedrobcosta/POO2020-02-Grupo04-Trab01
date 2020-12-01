94dce2fdce0d75df760b0becb47c0cfd3207a234
pedroeborges <48721021+pedroeborges@users.noreply.github.com>
2020-12-01 15:25:42 -0300

    Questão 01

---
+++ Questão 01/Aluno.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+{
+    class Aluno
+    {
+        private string nome = "Arthur Honório Brandão";
+        private string matricula = "11821ECP019";
+        private string tel = "(34) 9 9922 6624";
+
+        //constructor
+        //letra A
+
+        public Aluno ()
+        {
+            nome = "";
+            matricula = "";
+            tel = "";
+        }
+
+        public Aluno (string nome, string matricula, string tel)
+        {
+            this.nome = nome;
+            this.matricula = matricula;
+            this.tel = tel;
+        }
+
+        //metodos
+        //letra B
+        public void Membros()
+        {
+            Console.WriteLine("Digite o nome: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a matrícula: ");
+            matricula = Console.ReadLine();
+
+            Console.WriteLine("Digite o telefone: ");
+            tel = Console.ReadLine();
+        }
+
+        //letra C
+        public void Imprime()
+        {
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Matrícula: " + matricula);
+            Console.WriteLine("Telefone: " + tel);
+        }
+    }
+}
---
+++ Questão 01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Aluno A1 = new Aluno();
+
+            A1.Membros();
+
+            A1.Imprime();
+
+            Console.ReadKey();
+        }
+    }
+}
